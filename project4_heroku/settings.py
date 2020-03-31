77755a1133ca1838f363d20f4afff3b3c6f3e9eb
GannaSachkova <dorogaya_76@list.ru>
2020-03-31 02:19:12 +0000

    Removed STATIC_ROOT from settings.py

--- project4_heroku/settings.py
+++ project4_heroku/settings.py
@@ -157,9 +157,9 @@ STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, '/')
-STATIC_ROOT = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATIC_ROOT = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
